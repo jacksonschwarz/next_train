c2864eb7b9d1a2fd2faa6bc24fa28ebff1d97f9d
Jackson Schwarz <jackson@symphonylearning.com>
2017-09-06 17:07:58 -0400

    github pages commit

--- scripts/script.js
+++ scripts/script.js
@@ -16,12 +16,21 @@ let httpGet=function(url){
 }
 let callback=function(response){
     data=JSON.parse(response);
-    console.log(data.mode[0].route[0].direction[0].trip[0].pre_away)
-    let timeUntilArrival=Math.round(data.mode[0].route[0].direction[0].trip[0].pre_away/60)
-    let timeOfArrival=new Date(data.mode[0].route[0].direction[0].trip[0].pre_dt*1000)
-    document.write(`<h1 style="font-family:sans-serif;font-size:65px">
-    The next train arrives at <span style="color:green">Longwood Medical Area</span> in <br><span style="color:blue">${timeUntilArrival}</span> minutes, or at <span style="color:blue">${timeOfArrival.toString().substring(16, 21)}</span>
-    </h1>`)
+    console.log(data.mode[0].route[0])
+    if(data.mode[0].route[0].direction[1]){
+        let timeUntilArrival=Math.round(data.mode[0].route[0].direction[1].trip[0].pre_away/60)
+        let timeOfArrival=new Date(data.mode[0].route[0].direction[1].trip[0].pre_dt*1000)
+        document.write(`<h1 style="font-family:sans-serif;font-size:65px">
+        The next train arrives at <span style="color:green">Longwood Medical Area</span> in <br><span style="color:blue">${timeUntilArrival}</span> minutes, or at <span style="color:blue">${timeOfArrival.toLocaleString('en-US', { hour: 'numeric',minute:'numeric', hour12: true })}</span>
+        </h1>`)
+    }
+    else{
+        document.write(`<h1 style="font-family:sans-serif;font-size:65px">
+            There are currently no trains on schedule to arrive at Longwood Medical Area
+        </h1>
+        `)
+    }
+
     
 }
 let url="http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=place-lngmd&format=json"
