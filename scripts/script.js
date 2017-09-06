b31edc4caf10ef3ffcede162d998759688baa93b
Jackson Schwarz <jackson@symphonylearning.com>
2017-09-06 12:15:39 -0400

    Real initial commit

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+//reference:
+//Longwood Medical Area name: place-lgmd
+//pre_away is the paramater that represents the amount of time until the train arrives at the station
+//pre_dt is the time, in epoch time that the train is expected to arrive at the station
+//
+
+let httpGet=function(url){
+    var xmlHttp=new XMLHttpRequest();
+    xmlHttp.onreadystatechange=function(){
+        if(xmlHttp.readyState==4 && xmlHttp.status==200){
+            callback(xmlHttp.responseText)
+        } 
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send(null);
+}
+let callback=function(response){
+    data=JSON.parse(response);
+    console.log(data.mode[0].route[0].direction[0].trip[0].pre_away)
+    let timeUntilArrival=Math.round(data.mode[0].route[0].direction[0].trip[0].pre_away/60)
+    let timeOfArrival=new Date(data.mode[0].route[0].direction[0].trip[0].pre_dt*1000)
+    document.write(`<h1 style="font-family:sans-serif;font-size:65px">
+    The next train arrives at <span style="color:green">Longwood Medical Area</span> in <br><span style="color:blue">${timeUntilArrival}</span> minutes, or at <span style="color:blue">${timeOfArrival.toString().substring(16, 21)}</span>
+    </h1>`)
+    
+}
+let url="http://realtime.mbta.com/developer/api/v2/predictionsbystop?api_key=wX9NwuHnZU2ToO7GmGR9uw&stop=place-lngmd&format=json"
+let data=[]
+let check=function(){
+    httpGet(url);
+}
